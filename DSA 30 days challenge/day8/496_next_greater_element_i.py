d89a4752c0f99fc49b5ecf34d08521279a2e2ffa
Gajula Chenna Kesavulu <131852724+chennakesavulugajula@users.noreply.github.com>
2025-08-31 18:19:18 +0530

    Add files via upload

---
+++ DSA 30 days challenge/day8/496_next_greater_element_i.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = []
+        for num in nums1:
+            idx = nums2.index(num) 
+            next_greater = -1
+            for j in range(idx + 1, len(nums2)): 
+                if nums2[j] > num:
+                    next_greater = nums2[j]
+                    break
+            res.append(next_greater)
+        return res
